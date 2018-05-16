21c054a6d99c94c222ef8c1ae0b35f3afd48e9c7
Joana Jacome <jisjacome@gmail.com>
2018-05-16 17:03:32 +0200

    exercise with event clicks in changing css properties

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeBackground() {
+    let text = document.getElementById("para").style.backgroundColor = "yellow";
+}
+
+function backtoNormal() {
+    let newText = document.getElementById("para").style.backgroundColor = "";
+}
